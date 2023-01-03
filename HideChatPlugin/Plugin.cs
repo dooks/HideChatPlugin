9a0d4d620f94101698ec889ea2d12efb196ded30
Ziggy <dook@stardust.red>
2023-01-02 21:16:38 -0800

    Change to HideChatPlugin

---
+++ HideChatPlugin/Plugin.cs
@@ -0,0 +1,55 @@
+using Dalamud.Game;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiLib.Utilities;
+
+namespace HideChatPlugin
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "Hide Chat Plugin";
+        private BaseNode chatNode;
+
+        private GameGui GuiManager { get; init; }
+        public Framework FrameworkManager { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] GameGui guiManager,
+            [RequiredVersion("1.0")] Framework framework
+        )
+        {
+            this.GuiManager = guiManager;
+            this.FrameworkManager = framework;
+
+            this.chatNode = new BaseNode("ChatLog", this.GuiManager);
+            unsafe
+            {
+                this.chatNode.GetRootNode()->ToggleVisibility(true);
+            }
+            this.FrameworkManager.Update += OnFramework;
+        }
+
+        public void Dispose()
+        {
+            this.FrameworkManager.Update -= OnFramework;
+
+            unsafe
+            {
+                this.chatNode.GetRootNode()->ToggleVisibility(true);
+            }
+        }
+
+        public void OnFramework(Framework framework)
+        {
+            unsafe
+            {
+                // IDs found using /xldata ai
+                var visible = this.chatNode.GetComponentNode(5).GetNode<AtkResNode>(2)->IsVisible;
+                this.chatNode.GetRootNode()->ToggleVisibility(visible);
+            }
+        }
+    }
+}
